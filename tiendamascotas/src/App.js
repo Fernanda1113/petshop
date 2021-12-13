62a88438b19f0c8409589b43db28087d8fb25886
Fernanda1113 <erifer1113.ep@gmail.com>
2021-12-13 16:22:35 -0500

    prueba

--- tiendamascotas/src/App.js
+++ tiendamascotas/src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./Components/NavBar/NavBar"
 import Button from '@mui/material/Button'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 //import { makeStyles, AppBar, Toolbar, Typography } from '@mui/material'
-//import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
+import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <NavBar />
         <ItemListContainer />
-        
+        <ItemDetailContainer />
         <img src={'./logoCanhijo.png'}/>
         
        <Button>
---
+++ tiendamascotas/src/Components/ItemDetail/ItemDetail.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const ItemDetail = ({producto}) => {
+    return(
+        <article className='cardItem'>
+            <header className='header'>
+                <h2 className='itemHeader'>
+                    {producto?.name}
+                </h2>
+            </header>
+            <picture>
+                <img src={producto?.img} alt={producto?.name} className='imgItem'/>
+            </picture>
+            <section>
+                <p className='info'>
+                    Categoría: {producto?.category}
+                </p>
+                <p className='info'>
+                    Descripción: {producto?.description}
+                </p>
+                <p className='info'>
+                    Precio: {producto?.price}
+                </p>
+            </section>
+        </article>
+    )
+}
+
+export default ItemDetail
