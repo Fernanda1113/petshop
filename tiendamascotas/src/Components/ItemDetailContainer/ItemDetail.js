b865657420cbaaeb00d4e6e695fb468a24fbd27d
Fernanda1113 <erifer1113.ep@gmail.com>
2021-12-13 18:20:01 -0500

    IDC

--- tiendamascotas/src/Components/ItemDetailContainer/ItemDetail.js
+++ tiendamascotas/src/Components/ItemDetailContainer/ItemDetail.js
@@ -1,13 +1,29 @@
 import React from 'react'
 
 
-const ItemDetail = (productos) => {
+const ItemDetail = ({producto}) => {
     return(
-        <div>
-            <h2>{productos.id}</h2>
-            <p>{productos.description}</p>
-            <p>{productos.category}</p>
-        </div>
+        <article className='cardItem'>
+            <header className='header'>
+                <h2 className='itemHeader'>
+                    {producto?.name}
+                </h2>
+            </header>
+            <picture>
+                <img src={producto?.img} alt={producto?.name} className='imgItem'/>
+            </picture>
+            <section>
+                <p className='info'>
+                    Categoría: {producto?.category}
+                </p>
+                <p className='info'>
+                    Descripción: {producto?.description}
+                </p>
+                <p className='info'>
+                    Precio: {producto?.price}
+                </p>
+            </section>
+        </article>
     )
 }
 
