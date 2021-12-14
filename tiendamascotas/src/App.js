8877538fbb66f17e90621119ed1aee726e8bdba1
Fernanda1113 <erifer1113.ep@gmail.com>
2021-12-14 17:14:49 -0500

    eventos

--- tiendamascotas/src/App.js
+++ tiendamascotas/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from "./Components/NavBar/NavBar"
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import ItemCount from './Components/ItemCount/ItemCount';
-//import { makeStyles, AppBar, Toolbar, Typography } from '@mui/material'
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
 
 
