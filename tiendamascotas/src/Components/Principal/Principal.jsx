5348f11d0b72293dcc41b7480cc48448416b0ae7
Fernanda1113 <erifer1113.ep@gmail.com>
2022-01-05 19:31:19 -0500

    correcciones

--- tiendamascotas/src/Components/Principal/Principal.jsx
+++ tiendamascotas/src/Components/Principal/Principal.jsx
@@ -9,7 +9,7 @@ import Cart from '../Cart/Cart'
 const Principal = ({ greeting }) => {
 
     return (
-        <principal>
+        <Principal>
             <Switch>
 
                 <Route path="/" exact>
@@ -34,7 +34,7 @@ const Principal = ({ greeting }) => {
 
             </Switch>
 
-        </principal>
+        </Principal>
     )
 }
 
