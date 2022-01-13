be162f57aad3cd6d0cba5cdb793fb632aa501038
Rama D'Accorso <ramiro.daccorso@gmail.com>
2022-01-12 22:12:39 -0300

    Editado nombre de la coleccion

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
-//import Products from '../../productos';
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { getFirestore } from '../Firebase/Firebase'
 
